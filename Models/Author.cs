c2f8416a6038dd0098b88c9b048ad7ed3adba42d
Legion <Legion@ED>
2022-10-23 17:02:59 +0300

    Add project files.

---
+++ Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Pavel_Eduard_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
