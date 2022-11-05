aa33bc7f41b0758d32aca56b1c5f76f1b64e5b18
Mad-Ed <pavelcristianeduard22@yahoo.com>
2022-11-05 18:26:12 +0200

    f

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Pavel_Eduard_Lab2.Models;
+namespace Pavel_Eduard_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
